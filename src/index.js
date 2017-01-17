2cd46a3011de3a1b517800c743571700fc31aeae
Charles Wesley Cho <charleswcho@gmail.com>
2017-01-17 12:47:59 -0800

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './js/App';
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducer from './js/reducers'
+
+import App from './js/components/App';
 
 import './css/index.css';
 
+const middleware = [ thunk ]
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
